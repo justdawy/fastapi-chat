f9c6f75c99de9d6b12843b3c18adea771906848c
justdawy <davidshevnya@ukr.net>
2026-05-25 22:40:57 +0300

    chore: setup alembic

--- app/core/db.py
+++ app/core/db.py
@@ -1,11 +1,16 @@
 from sqlmodel import SQLModel, Session, create_engine,  select
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
 async_engine = create_async_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
-async def create_db_and_tables():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(
+        async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.core.config import settings
-from app.core.db import create_db_and_tables
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await create_db_and_tables()
     yield
 
 app = FastAPI(title=settings.APP_NAME, lifespan=lifespan)
